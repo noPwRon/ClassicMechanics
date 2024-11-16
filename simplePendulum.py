610899158d76136e2b1f6e6740be173c314e8bed
noPwRon <nopwron@gmail.com>
2024-11-15 22:27:19 -0800

    Started working on some of the basic equations

--- simplePendulum.py
+++ simplePendulum.py
@@ -1,1 +1,17 @@
-import vpython as vp
+import sympy as smp
+
+#  Variables
+
+# x = L*np.cos(theta)
+# y = L*np.sin(theta)
+
+# trying to figure out how to get T which is 1/2mv^2
+# our v^2 wiil be broken down into x and y components
+
+# L = 1/2*m*v^2
+
+
+m = smp.Symbol('m') # this one is easy and definintely needed
+L = smp.Symbol('L') # also another easy value because well... it's a value, but wwait this is where we want to use smp....
+x = smp.Symbol('x')
+y = smp.Symbol('y')
