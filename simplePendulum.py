ab46e913e061a1f2ad4de3a01b2f49420f48b805
noPwRon <nopwron@gmail.com>
2024-11-15 23:54:35 -0800

    Added a bunch more math that needs to be checked

--- simplePendulum.py
+++ simplePendulum.py
@@ -15,3 +15,27 @@ m = smp.Symbol('m') # this one is easy and definintely needed
 L = smp.Symbol('L') # also another easy value because well... it's a value, but wwait this is where we want to use smp....
 x = smp.Symbol('x')
 y = smp.Symbol('y')
+g = smp.Symbol('g') #gravity yall
+
+# but wait. we know more physics than that so lets gowith this much nicer choice
+
+# L = T- V
+
+# T = L*\Dot(Theta)
+# V = 1/2 m g h
+# h = L*cos(theta)
+theta = smp.Symbol(r'\theta')
+
+
+
+T = 0.5*m*L*thetaDot**2
+
+V = L*g*H
+
+L = T- V
+
+# need to find Dl = P
+
+
+# s
+
